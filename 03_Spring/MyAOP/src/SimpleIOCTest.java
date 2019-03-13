bdeb82c9721d13cc881da362c07815cf81bfa53c
copywang <noshow@qq.com>
2019-03-13 14:17:10 +0800

    day

---
+++ 03_Spring/MyAOP/src/SimpleIOCTest.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: SimpleIOCTest
+ * Author:   copywang
+ * Date:     2019/3/13 13:13
+ * Description: 测试类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package PACKAGE_NAME;
+
+public class SimpleIOCTest {
+}
